625ecdfc76c9e2eb701ec2bea38ed94059e2466b
scientific-dev <scientificguy007@gmail.com>
2025-12-06 23:34:29 +0530

    refactor: change variable names

--- salome.py
+++ salome.py
@@ -15,26 +15,26 @@ salome.salome_init()
 geom = geomBuilder.New()
 
 # Development input paramaters
-envelope_shape = "Wang"     # Shape of envelope
-envelope_length = 100       # Axial length of envelope
-envelope_resolution = 100   # Number of points taken axially
+envelope_shape = "LOTTE"        # Shape of envelope
+envelope_length = 100           # Axial length of envelope
+envelope_resolution = 100       # Number of points taken axially
 
-lobe_number = 3             # Number of lobes of envelope (< 3)
-lobe_offset_y = 13.333      # Distance between each lobe
-lobe_offset_x = 13.333      # Central lobe offset (for 3 lobe design)
-lobe_offset_z = 6           # Central lobe offset (for 3 lobe design)
-central_lobe_length = 80    # Length of central lobe (for 3 lobe design)
-central_lobe_shape = 'LOTTE'
+lobe_number = 3                 # Number of lobes of envelope (< 3)
+lobe_offset_y = 6               # Distance between each lobe
+lobe_offset_x = 13.333          # Central lobe offset (for 3 lobe design)
+lobe_offset_z = 6               # Central lobe offset (for 3 lobe design)
+central_lobe_length = 80        # Length of central lobe (for 3 lobe design)
+central_lobe_shape = "LOTTE"    # Central lobe shape having a different shape.
 
-fin_axial_offset = 80       # Distance between the leading edge of root chord of the fin and the nose. (< length - rc)
-fin_thickness = 12          # Thickness-to-chord ratio * 100
-fin_rc_length = 8           # Root chord length
-fin_section_resolution = 40 # Number of points taken axially for the airfoil cross section
-fin_taper_ratio = 0.5       # Ratio of tip chord length to root chord length
-fin_height = 5              # Perpendicular distance between root chord and the tip chord
-fin_sweep_angle = 0         # Angle of sweep with respect to the normal to the cross section.
-fin_number = 4              # Number of fins. Should be even if there are more than one lobes.
-fin_theta_pos = [45, 135]   # In case of more than one lobes, it is there to fix theta position of fins on each side of lobe.
+fin_axial_offset = 80           # Distance between the leading edge of root chord of the fin and the nose. (< length - rc)
+fin_thickness = 12              # Thickness-to-chord ratio * 100
+fin_rc_length = 8               # Root chord length
+fin_section_resolution = 40     # Number of points taken axially for the airfoil cross section
+fin_taper_ratio = 0.5           # Ratio of tip chord length to root chord length
+fin_height = 5                  # Perpendicular distance between root chord and the tip chord
+fin_sweep_angle = 0             # Angle of sweep with respect to the normal to the cross section.
+fin_number = 4                  # Number of fins. Should be even if there are more than one lobes.
+fin_theta_pos = [0, 90]         # Angle positions for the fin with respect to vertical axis at top. In case of more than one lobe, this will only represent the theta positions of only one side of half of the fins.
 
 # Modelling of envelope 
 
@@ -51,8 +51,8 @@ def create_envelope (shape, length):
     
     return gertler, envelope
 
-gertler1, envelope = create_envelope(envelope_shape, envelope_length)
-envelopes = [envelope] if lobe_number == 1 else [translate_object(envelope, 0, -1/2, 0), translate_object(envelope, 0, 1/2, 0)]
+main_gertler, envelope = create_envelope(envelope_shape, envelope_length)
+envelopes = [envelope] if lobe_number == 1 else [translate_object(envelope, 0, -1, 0), translate_object(envelope, 0, 1, 0)]
 
 if lobe_number == 3:
     central_lobe = envelope if central_lobe_length == envelope_length else create_envelope(central_lobe_shape, central_lobe_length)[1]
@@ -60,10 +60,10 @@ if lobe_number == 3:
 
 # Modelling of Fins
 
+fin_axial_offset = min(fin_axial_offset, envelope_length - fin_rc_length)
+fin_radial_offset = main_gertler.at(fin_axial_offset)
 rc_axial_offset = fin_axial_offset
 tc_axial_offset = fin_rc_length/2 * (1 - fin_taper_ratio) + fin_height * np.tan(np.radians(fin_sweep_angle))
-fin_axial_offset = min(fin_axial_offset, envelope_length - fin_rc_length)
-fin_radial_offset = gertler1.at(fin_axial_offset)
 rc_vertices = []
 tc_vertices = []
 
@@ -78,14 +78,14 @@ rc_face = geom.MakeFace(rc_wire, True)
 tc_face = geom.MakeFace(tc_wire, True)
 
 midchord_direction = [geom.MakeVertex(0, 0, 0), geom.MakeVertex(tc_axial_offset, 0, fin_height)]
-fin_surface = geom.MakePipeWithDifferentSectionsBySteps(
+planform_surface = geom.MakePipeWithDifferentSectionsBySteps(
     [rc_wire, tc_wire],
     midchord_direction, 
     geom.MakePolyline(midchord_direction, False), 
 )
 
-x2, z2 = gertler1.get_trailing_edge_intercept(fin_axial_offset, fin_rc_length)
-fin = geom.MakeSolid(geom.MakeShell([fin_surface, rc_face, tc_face]))
+x2, z2 = main_gertler.get_trailing_edge_intercept(fin_axial_offset, fin_rc_length)
+fin = geom.MakeSolid(geom.MakeShell([planform_surface, rc_face, tc_face]))
 fin = geom.MakeRotationThreePoints(
     fin, 
     geom.MakeVertex(fin_axial_offset, 0, fin_radial_offset),
@@ -96,14 +96,14 @@ fin = geom.MakeRotationThreePoints(
 fins = []
 
 if lobe_number == 1:
-    fins = [geom.MakeRotation(fin, x_axis, i * (2 * np.pi / fin_number)) for i in range(0, fin_number)]
-else:
-    if fin_number % 2 != 0:
-        fin_number -= 1
+    if not fin_theta_pos:
+        fin_theta_pos = [i * (360 / fin_number) for i in range(0, fin_number)]
 
-    for i in range(0, int(fin_number/2)):
-        fins.append(translate_object(geom.MakeRotation(fin, x_axis, np.radians(fin_theta_pos[i])), 0, -1/2, 0))
-        fins.append(translate_object(geom.MakeRotation(fin, x_axis, np.radians(-fin_theta_pos[i])), 0, 1/2, 0))
+    fins = [geom.MakeRotation(fin, x_axis, -np.pi/2 + np.radians(theta)) for theta in fin_theta_pos]
+else:
+    for theta in fin_theta_pos:
+        fins.append(translate_object(geom.MakeRotation(fin, x_axis, np.radians(theta)), 0, -1, 0))
+        fins.append(translate_object(geom.MakeRotation(fin, x_axis, np.radians(-theta)), 0, 1, 0))
 
 airship = geom.MakeFuseList(envelopes + fins)
 airship_id = geom.addToStudy(airship, "Airship")
